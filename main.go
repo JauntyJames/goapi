d9ac2542f198ca45d4f3049ee11ee13b097dd2a1
James Hartman <james.hartman@oracle.com>
2019-03-08 19:07:42 -0500

    Implemented tests

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"log"
 	"os"
-
-	"github.com/gorilla/mux"
 )
-
-func (a *App) Initialize(user, password, dbname string) {
-	connectionString :=
-		fmt.Springf("user=%s password=%s dbname=%s"-user, password, dbname)
-
-	var err error
-	a.DB, err = sql.Open("postgres", connectionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	a.Router = mux.NewRouter
-}
 
 func main() {
 	a := App{}
