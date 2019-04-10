1a77559b9226336bacde854748677fd0c3b7e31b
justinm-nyc <justin.martinelli@oracle.com>
2019-04-10 15:35:04 -0400

    added functions and test

--- app/app.go
+++ app/app.go
@@ -47,6 +47,18 @@ func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.getProduct).Methods("GET")
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.updateProduct).Methods("PUT")
 	a.Router.HandleFunc("/product/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
+
+	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
+	//Returns all symbols
+	a.Router.HandleFunc("/symbols", a.getSymbols).Methods("GET")
+	//Returns portfolio of specified user
+	a.Router.HandleFunc("/portfolio/{id:[0-9]+}", a.getPortfolio).Methods("GET")
+	//Adds specified symbol into user portfolio from the userID given in the payload
+	a.Router.HandleFunc("/addwatch/portfolio/{id:[0-9]+}", a.addPortfolioEntry).Methods("POST")
+	//Sets portfolio entry to not watched for specified symbol ID,using the userID given in the payload
+	a.Router.HandleFunc("/removewatch/portfolio/{id:[0-9]+}", a.setWatchedFalse).Methods("PUT")
+	//Returns news pertaining to specified user
+	a.Router.HandleFunc("/news/{id:[0-9]+}", a.getNews).Methods("GET")
 }
 
 func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
@@ -104,7 +116,6 @@ func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-
 	respondWithJSON(w, http.StatusCreated, p)
 }
 
@@ -161,3 +172,158 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	w.Write(response)
 }
+
+//Trading App Functions
+
+func (a *App) createUser(w http.ResponseWriter, r *http.Request) {
+	var u User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&u); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := u.createUser(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, u)
+
+}
+
+func (a *App) addPortfolioEntry(w http.ResponseWriter, r *http.Request) {
+	var portfolio Portfolio
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid symbol ID")
+		return
+	}
+	portfolio.SymbolID = id
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&portfolio); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := portfolio.createPortfolioEntry(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, portfolio)
+}
+
+func (a *App) getNews(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
+	}
+
+	p := Portfolio{UserID: id}
+
+	news, err := p.getNews(a.DB, 0, 10)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "News not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, news)
+}
+
+func (a *App) getPortfolio(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
+	}
+
+	p := Portfolio{UserID: id}
+
+	portfolio, err := p.getPortfolio(a.DB)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Portfolio not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, portfolio)
+}
+
+func (a *App) getSymbols(w http.ResponseWriter, r *http.Request) {
+	symbols, err := getSymbols(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, symbols)
+}
+
+func (a *App) setWatchedFalse(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid symbol ID")
+		return
+	}
+
+	var p Portfolio
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	p.SymbolID = id
+
+	if err := p.setWatchedFalse(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+// func (a *App) createAccount(w http.ResponseWriter, r *http.Request) {
+// 	var accnt Account
+// 	decoder := json.NewDecoder(r.Body)
+// 	if err := decoder.Decode(&accnt); err != nil {
+// 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+// 		return
+// 	}
+// 	defer r.Body.Close()
+
+// 	if err := accnt.createAccount(a.DB); err != nil {
+// 		respondWithError(w, http.StatusInternalServerError, err.Error())
+// 		return
+// 	}
+
+// 	respondWithJSON(w, http.StatusCreated, accnt)
+
+// }
